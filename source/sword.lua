2c3cbdfd2249a4d3a13fc2a6e7e3e94eb4698164
Daniel Fich <danielfich2@gmail.com>
2026-09-25 10:11:29 +0200

    Changed value back

--- source/sword.lua
+++ source/sword.lua
@@ -8,8 +8,8 @@ local playerPosX = 200
 local playerPosY = 120
 
 -- Settings
-MinSwipeEnterSpeed = 20 -- 25
-MinSwipeSpeed = 7 -- 7
+MinSwipeEnterSpeed = 40 -- 25
+MinSwipeSpeed = 14 -- 7
 --local minSwordSwipeDegSpeed = 40
 local maxSwordDegSpeed = 50
 OffsetLength = 50
